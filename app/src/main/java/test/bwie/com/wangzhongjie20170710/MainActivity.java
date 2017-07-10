7d2d6bead8d9651fd671f213ff615055b48161ad
dell <1727226822@>
2017-07-10 09:35:39 +0800

    上传代码

--- app/src/main/java/test/bwie/com/wangzhongjie20170710/MainActivity.java
+++ app/src/main/java/test/bwie/com/wangzhongjie20170710/MainActivity.java
@@ -2,13 +2,26 @@ package test.bwie.com.wangzhongjie20170710;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView pw_jiaimg;
+    private TextView pw_num;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.jj);
+        pw_jiaimg = (ImageView) findViewById(R.id.pw_jiaimg);
+        pw_num = (TextView) findViewById(R.id.pw_num);
+         pw_jiaimg.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                  pw_num.setText("2");
+             }
+         });
     }
 }
