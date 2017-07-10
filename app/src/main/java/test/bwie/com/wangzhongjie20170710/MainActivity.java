92133d52967a3d7181634da46972d4816c0dc5a8
dell <1727226822@>
2017-07-10 09:24:28 +0800

    Initial commit

---
+++ app/src/main/java/test/bwie/com/wangzhongjie20170710/MainActivity.java
@@ -0,0 +1,14 @@
+package test.bwie.com.wangzhongjie20170710;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
